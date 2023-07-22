5c1143b926b3dfdf297d733f2843177c37314a7a
muath2332 <138536460+muath2332@users.noreply.github.com>
2023-07-22 06:39:03 +0300

    Delete اذكارك.js

--- اذكارك.js
+++
@@ -1,8 +0,0 @@
-// Keeping state in DOM is NOT recommended, but here we go...
-var count = document.getElementById('count');
-
-function plusOne() {
-  var number = Number(count.textContent);
-  number++;
-  count.textContent = number.toString();
-}
